513ecf4ace0e13108c19d774c1921a7e66e3a36c
theyoungbeast <theyoungbeast>
2021-12-06 01:18:59 +0100

    css

--- cinema-app/src/Movies/RemoveMovie.js
+++ cinema-app/src/Movies/RemoveMovie.js
@@ -14,9 +14,11 @@ function RemoveMovie(props)
 
     const onNoClick = () => navigate('../../' + id);
 
-    return (<div>
-        <p>Would you like to remove: {props.movies[id].title} ?</p>
-        <button onClick={ onYesClick }>Yes</button> | <button onClick={ onNoClick }>No</button>
+    return (<div className="main-container">
+        <div>
+            <p>Would you like to remove: {props.movies[id].title} ?</p>
+            <button onClick={ onYesClick }>Yes</button> | <button onClick={ onNoClick }>No</button>
+        </div>
     </div>);
 }
 
